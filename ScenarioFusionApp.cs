b52d07448f94889ba0fce9490e1b5d11dad9c4e7
SormazPredrag <sormazpredrag@gmail.com>
2024-04-19 16:25:14 +0200

    Update ScenarioFusionApp.cs

--- ScenarioFusionApp.cs
+++ ScenarioFusionApp.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -28,6 +30,10 @@ namespace SimulationToolTest
             builder.MoveToElement(FusionApp, 1190, 414).Click().Build().Perform(); // pixel offset from top left
             builder.DragAndDropToOffset(FusionApp, 200, 100).Perform();
             Thread.Sleep(TimeSpan.FromSeconds(4));
+
+            // Take a screenshot
+            Screenshot screenshot = sessionHTT.GetScreenshot();
+            screenshot.SaveAsFile("D:\\testHistoScreenshot.png");
 
             app.ShutDownMenuClick();
             
