22dd7ce76a07d1176882cccd8540ba6a50fdc6e3
SormazPredrag <sormazpredrag@gmail.com>
2024-05-24 17:39:02 +0200

    ScenarioTreatmentHead

---
+++ ScenarioTreatmentHead.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+
+namespace SimulationToolTest
+{
+
+    [TestClass]
+    public class ScenarioTreatmentHead : HistoTraningSession
+    {
+        private string targetName = "new target";
+
+        [TestMethod]
+        public void TreatmentHeadChange()
+        {
+            var app = new ScenarioPatientOpen();
+            app.ExpandExsistingPatientList();
+
+            Thread.Sleep(TimeSpan.FromSeconds(4));
+
+            //Add Target button
+            //Button[@Name=\"Add Target\"][@AutomationId=\"PlanWidget.frame.planBodyWidget.addTargetButton\"]"
+            sessionHTT.FindElementByAccessibilityId("PlanWidget.frame.planBodyWidget.addTargetButton").Click();
+
+            //WindowsElement FusionApp = sessionHTT.FindElementByName("Fusion App");
+            string xpathFusion = "//Window[@ClassName=\"QDialog\"][@Name=\"Fusion App\"]/Group[@AutomationId=\"TargetInfoEditWidget\"]";
+            WindowsElement FusionApp = sessionHTT.FindElementByXPath(xpathFusion);
+            WindowsElement targetNameEdit = FusionApp.FindElementByAccessibilityId("TargetInfoEditWidget.nameEdit") as WindowsElement;
+            targetNameEdit.Click();
+            targetNameEdit.Clear();
+            targetNameEdit.SendKeys(targetName);
+            //WindowsElement startTarget = FusionApp.FindElementByAccessibilityId("TargetInfoEditWidget.saveButton") as WindowsElement;
+            WindowsElement startTarget = FusionApp.FindElementByAccessibilityId("TargetInfoEditWidget.startButton") as WindowsElement;
+            startTarget.Click();
+
+            sessionHTT.FindElementByAccessibilityId("MainWindow.centralwidget.stackedWidget.patientRecordPage.stackedWidget.treatmentPlanPage.scrollArea.qt_scrollarea_viewport.scrollAreaWidgetContents.treatmentPlanControllerBg.TreatmentPlanController.targetControlsWidget.treatmentHeadWidget.comboBoxTransducer").Click();
+            //Name:	"2-12 cm"
+            sessionHTT.FindElementByName("8-14 cm").Click();
+            //Proveriti da li se TH pomerio
+
+            app.ShutDownMenuClick();
+
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+    }
+
+}
