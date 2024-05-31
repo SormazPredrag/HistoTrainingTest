cd1fa33a6dd1f4ba92b6d5fbd111666965f9255f
SormazPredrag <sormazpredrag@gmail.com>
2024-05-31 15:49:57 +0200

    Update Scenario3DManipulation.cs

--- Scenario3DManipulation.cs
+++ Scenario3DManipulation.cs
@@ -14,7 +14,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using static System.Net.Mime.MediaTypeNames;
 
 namespace SimulationToolTest
 {
@@ -22,7 +21,8 @@ namespace SimulationToolTest
     public class Scenario3DManipulation : HistoTraningSession
     {
         private string screenFileName = "D:\\HistoFusionAppScreenshot.png";
-
+        private string screenFileName_1 = "D:\\HistoFusionAppScreenshot_1.png";
+        
         [TestMethod]
         [DataRow("new target", 5)]
         public void Manipupation3D(string targetName, int browserPause)
@@ -32,12 +32,41 @@ namespace SimulationToolTest
 
             Thread.Sleep(TimeSpan.FromSeconds(4));
 
+            //Add Target button
+            //Button[@Name=\"Add Target\"][@AutomationId=\"PlanWidget.frame.planBodyWidget.addTargetButton\"]"
+            sessionHTT.FindElementByAccessibilityId("PlanWidget.frame.planBodyWidget.addTargetButton").Click();
             WindowsElement FusionApp = sessionHTT.FindElementByName("Fusion App");
+            //"/Pane[@ClassName=\"#32769\"][@Name=\"Desktop 1\"]/Window[@Name=\"Histosonics Training Tool\"][@AutomationId=\"MainWindow\"]/Window[@ClassName=\"QDialog\"][@Name=\"Fusion App\"]/Group[@AutomationId=\"TargetInfoEditWidget\"]/Edit[@AutomationId=\"TargetInfoEditWidget.nameEdit\"]"
+            WindowsElement targetNameEdit = FusionApp.FindElementByAccessibilityId("TargetInfoEditWidget.nameEdit") as WindowsElement;
+            targetNameEdit.Click();
+            targetNameEdit.Clear();
+            targetNameEdit.SendKeys(targetName);
+            //WindowsElement startTarget = FusionApp.FindElementByAccessibilityId("TargetInfoEditWidget.saveButton") as WindowsElement;
+            WindowsElement startTarget = FusionApp.FindElementByAccessibilityId("TargetInfoEditWidget.startButton") as WindowsElement;
+            startTarget.Click();
+
+            /*
+            //WindowsElement Lateral = sessionHTT.FindElementByName("Treatment Head Angles");
+            WindowsElement Lateral = sessionHTT.FindElementByAccessibilityId("MainWindow.centralwidget.stackedWidget.patientRecordPage.stackedWidget.treatmentPlanPage.scrollArea.qt_scrollarea_viewport.scrollAreaWidgetContents.treatmentPlanControllerBg.TreatmentPlanController.targetControlsWidget.groupBoxAngles.horizontalSliderAngleZ");
+            var builder1 = new Actions(sessionHTT);
+            xCoord = Lateral.Size.Width / 2;
+            int yCord = Lateral.Size.Height / 2;
+            builder1.MoveToElement(Lateral, xCoord, yCord).Click().Build().Perform(); // pixel offset from top left
+            builder1.DragAndDropToOffset(Lateral, 30, 0).Perform();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            WindowsElement CranioCaudal = sessionHTT.FindElementByAccessibilityId("MainWindow.centralwidget.stackedWidget.patientRecordPage.stackedWidget.treatmentPlanPage.scrollArea.qt_scrollarea_viewport.scrollAreaWidgetContents.treatmentPlanControllerBg.TreatmentPlanController.targetControlsWidget.groupBoxAngles.horizontalSliderAngleX");
+            var builder2 = new Actions(sessionHTT);
+            //builder2.MoveToElement(CranioCaudal, xCoord, yCord).Click().Build().Perform(); // pixel offset from top left
+            builder2.DragAndDropToOffset(CranioCaudal, -40, 0).Perform();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            */
+
+            FusionApp = sessionHTT.FindElementByName("Fusion App");
             int x = FusionApp.Location.X;
             int y = FusionApp.Location.Y;
             int dx = FusionApp.Size.Width;
             int dy = FusionApp.Size.Height;
-
 
             var builder = new Actions(sessionHTT);
             int xCoord = 1190 * WinWidth / 1680;
@@ -67,8 +96,8 @@ namespace SimulationToolTest
 
             //Rotate view
             var builder_app = new Actions(sessionHTT);
-            xCoord = FusionApp.Size.Width / 2;
-            yCoord = FusionApp.Size.Height / 2;
+            xCoord = FusionApp.Size.Width / 2 + 5;
+            yCoord = FusionApp.Size.Height / 2 + 5;
             builder_app.MoveToElement(FusionApp, xCoord, yCoord)
                 .Click()
                 .ClickAndHold()
@@ -79,8 +108,6 @@ namespace SimulationToolTest
 
             //Translate
             builder_app = new Actions(sessionHTT);
-            xCoord = FusionApp.Size.Width / 2;
-            yCoord = FusionApp.Size.Height / 2;
             builder_app.MoveToElement(FusionApp, xCoord, yCoord)
                 .Click()
                 .ClickAndHold()
@@ -89,6 +116,28 @@ namespace SimulationToolTest
                 .Build().Perform();
             Thread.Sleep(TimeSpan.FromSeconds(1));
 
+            /*//Mouse whell do not work
+            var tactions = new TouchActions(sessionHTT); //tactions.scroll(10, 10) tactions.perform()
+            tactions.Scroll(0, 20).Perform();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            */
+
+            // Take a screenshot
+            //Iz nekog razloga ne mozemo uzeti screenshot Fusion App elementa pa je treba iseci iz cele slike
+            screenshot = sessionHTT.GetScreenshot();
+
+            //ConvertTo Image
+            pic = new Mat();
+            CvInvoke.Imdecode(screenshot.AsByteArray, LoadImageType.AnyColor, pic);
+            Mat cropped_1 = new Mat(pic, rectangle);
+            cropped_1.Save(screenFileName_1);
+
+            /*//Razlika
+            Mat ResultImage = new Mat();
+            CvInvoke.AbsDiff(cropped, cropped_1, ResultImage);
+            int diff = CvInvoke.CountNonZero(ResultImage);
+            Console.WriteLine($"Razlika je: {diff.ToString()}");
+            */
 
             /*
             //Minimize 3D 
@@ -101,37 +150,6 @@ namespace SimulationToolTest
 
             //IList<WindowsElement> static trt = sessionHTT.FindElements(By.Name("VIBE DINAMICO 4 MEDIDAS 20 seg"));
 
-            //Add Target button
-            //Button[@Name=\"Add Target\"][@AutomationId=\"PlanWidget.frame.planBodyWidget.addTargetButton\"]"
-            sessionHTT.FindElementByAccessibilityId("PlanWidget.frame.planBodyWidget.addTargetButton").Click();
-            FusionApp = sessionHTT.FindElementByName("Fusion App");
-            //"/Pane[@ClassName=\"#32769\"][@Name=\"Desktop 1\"]/Window[@Name=\"Histosonics Training Tool\"][@AutomationId=\"MainWindow\"]/Window[@ClassName=\"QDialog\"][@Name=\"Fusion App\"]/Group[@AutomationId=\"TargetInfoEditWidget\"]/Edit[@AutomationId=\"TargetInfoEditWidget.nameEdit\"]"
-            WindowsElement targetNameEdit = FusionApp.FindElementByAccessibilityId("TargetInfoEditWidget.nameEdit") as WindowsElement;
-            targetNameEdit.Click();
-            targetNameEdit.Clear();
-            targetNameEdit.SendKeys(targetName);
-            //WindowsElement startTarget = FusionApp.FindElementByAccessibilityId("TargetInfoEditWidget.saveButton") as WindowsElement;
-            WindowsElement startTarget = FusionApp.FindElementByAccessibilityId("TargetInfoEditWidget.startButton") as WindowsElement;
-            startTarget.Click();
-
-
-            
-            /*
-            //WindowsElement Lateral = sessionHTT.FindElementByName("Treatment Head Angles");
-            WindowsElement Lateral = sessionHTT.FindElementByAccessibilityId("MainWindow.centralwidget.stackedWidget.patientRecordPage.stackedWidget.treatmentPlanPage.scrollArea.qt_scrollarea_viewport.scrollAreaWidgetContents.treatmentPlanControllerBg.TreatmentPlanController.targetControlsWidget.groupBoxAngles.horizontalSliderAngleZ");
-            var builder1 = new Actions(sessionHTT);
-            xCoord = Lateral.Size.Width / 2;
-            int yCord = Lateral.Size.Height / 2;
-            builder1.MoveToElement(Lateral, xCoord, yCord).Click().Build().Perform(); // pixel offset from top left
-            builder1.DragAndDropToOffset(Lateral, 30, 0).Perform();
-            Thread.Sleep(TimeSpan.FromSeconds(1));
-
-            WindowsElement CranioCaudal = sessionHTT.FindElementByAccessibilityId("MainWindow.centralwidget.stackedWidget.patientRecordPage.stackedWidget.treatmentPlanPage.scrollArea.qt_scrollarea_viewport.scrollAreaWidgetContents.treatmentPlanControllerBg.TreatmentPlanController.targetControlsWidget.groupBoxAngles.horizontalSliderAngleX");
-            var builder2 = new Actions(sessionHTT);
-            //builder2.MoveToElement(CranioCaudal, xCoord, yCord).Click().Build().Perform(); // pixel offset from top left
-            builder2.DragAndDropToOffset(CranioCaudal, -40, 0).Perform();
-            Thread.Sleep(TimeSpan.FromSeconds(1));
-            */
 
             //Sidebar
             // "MainWindow.centralwidget.stackedWidget.patientRecordPage.displayAndControlsWidget.displayStackedWidget.displayPageContainer.viewSidebar"
